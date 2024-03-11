7f811a9ac840d33dfc7e364760a8ed38c6567bd6
Mohamed El Bakhkhouch <med.bakhkhouch@gmail.com>
2024-03-11 13:33:22 +0000

    hello

---
+++ includes/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dedsec <dedsec@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 15:04:52 by kael-ala          #+#    #+#             */
+/*   Updated: 2024/03/07 01:05:23 by dedsec           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_strdup(char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strchr(char *s, int c);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
+
+#endif
